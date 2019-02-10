c747e62917d51948ce217e87d3c03f7f86623498
Sung Kim <sungmkim80@gmail.com>
2019-02-10 15:03:24 -0500

    🛠 Removed explicit return from useLocalStorage hook

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import React from "react";
 
-export default function useLocalStorage(key: string, initialValue: string = "") : [string, (item: string) => void] {
+export default function useLocalStorage(key: string, initialValue: string = "") {
   const [item, setValue] = React.useState(() => {
     const value = localStorage.getItem(key) || initialValue;
     localStorage.setItem(key, value);
